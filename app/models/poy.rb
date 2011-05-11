86fce4cf30e6f46596831d0a6a4ba51e46646eec
Jacob <jacob@jigglypuff.(none)>
2011-05-11 15:33:09 -0400

    clean up some code

--- app/models/poy.rb
+++ app/models/poy.rb
@@ -87,65 +87,65 @@ class Poy
 
 
   end
-
-  def self.run_all(job )
-       job.service_id = PoyService.init
-    job.status = "starting poy service"
-    job.save
-
-
-      @poy_script =""
-      fasta = JobFile.where("file_type = 'fas' and job_id=#{job.id}")[0]
-      PoyService.add_text_file(job.service_id,fasta.name,fasta.data)
-      #PoyService.add_file(job.service_id,fasta.name,fasta.data)
-      if(job.prealigned_fasta)
-        fasta[:file_type]="ia"
-        fasta.save
-        @poy_script <<"read(prealigned:(\"#{fasta.name}\",tcm:(1,2)))\n"
-        @poy_script <<"set (root:\"#{job.outgroup}\")\n"
-     else
-        @poy_script <<"read (\"#{fasta.name}\")\n"
-        @poy_script <<"set (root:\"#{job.outgroup}\")\n"
-        @poy_script <<"transform (tcm:(1, 2))\n"
-      end
-
-
-      if(job.supplied_tree)
-        tree= JobFile.where("file_type = 'tre' and job_id=#{job.id}")[0]
-        PoyService.add_text_file(job.service_id,tree.name,tree.data)
-        tree_name =    tree.name
-        @poy_script << "read(\"#{tree.name}\")\n"
-        @poy_script << "transform(static_approx)\n"
-      else
-        tree_name = "#{job.name}.tre"
-        @poy_script << "search(max_time:00:0:5, memory:gb:2)\n"
-        @poy_script << "transform(static_approx)\n"
-        @poy_script << "select(best:1)\n"
-        @poy_script << "report (\"#{job.name}.tre\", trees)\n"
-      end
-
-      if(!job.prealigned_fasta)
-         @poy_script << "report (\"#{job.name}.ia\", ia)\n"
-      end
-
-      @poy_script << "report(\"#{job.name}.poy_output\",data,diagnosis)\n"
-      @poy_script << "exit()\n"
-      @poy_file = JobFile.new(:job_id =>job.id, :file_type=>"poy",:name => job.name+".poy",  :data => @poy_script)
-      @poy_file.save
-      PoyService.add_text_file(job.service_id,@poy_file.name,@poy_file.data)
-
-       job.status = "uploaded all poy files"
-       job.save
-
-       begin
-         PoyService.submit_poy(job.service_id)
-         #PoyService.submit_phenGen(job.service_id,job.name,  tree_name)
-      rescue Exception => ex
-      job.status = "poy failed"
-      job.error_output = ex.message+ex.backtrace.to_s
-      job.save
-      exit
-    end
-      job.save
-  end
+#
+#  def self.run_all(job )
+#       job.service_id = PoyService.init
+#    job.status = "starting poy service"
+#    job.save
+#
+#
+#      @poy_script =""
+#      fasta = JobFile.where("file_type = 'fas' and job_id=#{job.id}")[0]
+#      PoyService.add_text_file(job.service_id,fasta.name,fasta.data)
+#      #PoyService.add_file(job.service_id,fasta.name,fasta.data)
+#      if(job.prealigned_fasta)
+#        fasta[:file_type]="ia"
+#        fasta.save
+#        @poy_script <<"read(prealigned:(\"#{fasta.name}\",tcm:(1,2)))\n"
+#        @poy_script <<"set (root:\"#{job.outgroup}\")\n"
+#     else
+#        @poy_script <<"read (\"#{fasta.name}\")\n"
+#        @poy_script <<"set (root:\"#{job.outgroup}\")\n"
+#        @poy_script <<"transform (tcm:(1, 2))\n"
+#      end
+#
+#
+#      if(job.supplied_tree)
+#        tree= JobFile.where("file_type = 'tre' and job_id=#{job.id}")[0]
+#        PoyService.add_text_file(job.service_id,tree.name,tree.data)
+#        tree_name =    tree.name
+#        @poy_script << "read(\"#{tree.name}\")\n"
+#        @poy_script << "transform(static_approx)\n"
+#      else
+#        tree_name = "#{job.name}.tre"
+#        @poy_script << "search(max_time:00:0:5, memory:gb:2)\n"
+#        @poy_script << "transform(static_approx)\n"
+#        @poy_script << "select(best:1)\n"
+#        @poy_script << "report (\"#{job.name}.tre\", trees)\n"
+#      end
+#
+#      if(!job.prealigned_fasta)
+#         @poy_script << "report (\"#{job.name}.ia\", ia)\n"
+#      end
+#
+#      @poy_script << "report(\"#{job.name}.poy_output\",data,diagnosis)\n"
+#      @poy_script << "exit()\n"
+#      @poy_file = JobFile.new(:job_id =>job.id, :file_type=>"poy",:name => job.name+".poy",  :data => @poy_script)
+#      @poy_file.save
+#      PoyService.add_text_file(job.service_id,@poy_file.name,@poy_file.data)
+#
+#       job.status = "uploaded all poy files"
+#       job.save
+#
+#       begin
+#         PoyService.submit_poy(job.service_id)
+#         #PoyService.submit_phenGen(job.service_id,job.name,  tree_name)
+#      rescue Exception => ex
+#      job.status = "poy failed"
+#      job.error_output = ex.message+ex.backtrace.to_s
+#      job.save
+#      exit
+#    end
+#      job.save
+#  end
 end
